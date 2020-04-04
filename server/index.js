acec05b2c85299d8338ef15d0d36137ba20c3bfb
Constantine <constantinesan77@users.noreply.github.com>
2020-04-04 16:53:29 +0200

    Dodano logowanie

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 const Koa = require('koa')
+const logger = require('koa-logger')
 const serve = require('koa-static')
+const favicon = require('koa-favicon')
+
 
 const app = new Koa()
 const port = process.env.PORT || 3000
+//Dodawanie faviconu
+app.use(favicon('./client/favicon.ico'))
+// Logowanie wszystkich żądań na konsoli
+app.use(logger())
 
 app.use(serve('client'))
 
